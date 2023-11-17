157f9447d194e829412d55b04f3f983bb9a4b8d7
Jan Sobanski <sobanski.jan2@gmail.com>
2023-11-18 00:00:14 +0100

    Configure the database module

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true, // This should be changed to false once the MVP is done
+    })
+  ]
+})
+export class DatabaseModule {}
