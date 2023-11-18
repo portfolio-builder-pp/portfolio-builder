50a3346423275800984f69462e824755ea4883fd
Jan Sobanski <sobanski.jan2@gmail.com>
2023-11-18 14:42:24 +0100

    Update user service methods

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { InternalUserDto } from '@portfolio-builder/shared-types';
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
 
-  findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+  findAll(): Promise<InternalUserDto[]> {
+    return this.usersRepository.find({});
   }
 
-  findOne(id: number): Promise<User | null> {
+  findOne(id: number): Promise<InternalUserDto | null> {
     return this.usersRepository.findOneBy({ id });
+  }
+
+  async create(userDetails: InternalUserDto): Promise<InternalUserDto> {
+    return this.usersRepository.save(userDetails);
   }
 
   async remove(id: number): Promise<void> {
