4c9bd41b3d2c3a71bfbebf86f6e2430ea4c5d0d7
Jan Sobanski <sobanski.jan2@gmail.com>
2023-11-18 14:42:51 +0100

    Update user module

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,10 +2,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { UserMapper } from './user.mapper';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  providers: [UserService, UserMapper],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
