e98aa700c7636f48c187c012f5b0352edc5cca69
Jan Sobanski <sobanski.jan2@gmail.com>
2023-11-21 20:35:20 +0100

    Change user id to uuid

--- apps/api/src/user/user.entity.ts
+++ apps/api/src/user/user.entity.ts
@@ -3,8 +3,8 @@ import { InternalUserDto } from '@portfolio-builder/shared-types';
 
 @Entity()
 export class User implements InternalUserDto {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   firstName: string;
