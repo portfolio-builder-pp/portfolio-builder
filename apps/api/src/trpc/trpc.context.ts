9f0e7453230b1d7273596f1196c2918d471380cf
Jan Sobanski <sobanski.jan2@gmail.com>
2023-12-01 23:34:04 +0100

    Add context to trpc

--- apps/api/src/trpc/trpc.context.ts
+++ apps/api/src/trpc/trpc.context.ts
@@ -3,12 +3,13 @@ import { UserDto } from "@portfolio-builder/shared-types";
 
 declare module 'express-session' {
   interface SessionData {
-    user?: UserDto,
+    user?: UserDto | null,
   }
 }
 
 export const createContext = ({ req }: CreateExpressContextOptions) => {
   return {
+    session: req.session,
     isAuthenticated: !!req.session?.user,
     role: req.session?.user?.role ?? null,
   }
--- apps/api/src/trpc/trpc.service.ts
+++ apps/api/src/trpc/trpc.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { initTRPC } from '@trpc/server';
+import { createContext } from './trpc.context';
+
+type Context = ReturnType<typeof createContext>
 
 @Injectable()
 export class TrpcService {
-  private readonly trpc = initTRPC.create();
+  private readonly trpc = initTRPC.context<Context>().create();
   public readonly procedure = this.trpc.procedure;
   public readonly router = this.trpc.router;
   public readonly mergeRouters = this.trpc.mergeRouters;
