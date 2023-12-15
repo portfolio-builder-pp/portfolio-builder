7159f9b4e7c9f669e5958508018c64fe12bf18d6
Jan Sobanski <sobanski.jan2@gmail.com>
2023-12-15 20:23:47 +0100

    Use the config module throughout the app

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Environments } from '@portfolio-builder/shared-types';
+import { AppConfigService, ConfigModule } from '../config';
 import { User } from '../user/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
-      synchronize: process.env.NODE_ENV === 'development',
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: AppConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host', { infer: true }),
+        port: configService.get('database.port', { infer: true }),
+        username: configService.get('database.username', { infer: true }),
+        password: configService.get('database.password', { infer: true }),
+        database: configService.get('database.name', { infer: true }),
+        synchronize: configService.get('general.environment', { infer: true }) === Environments.Development,
+        entities: [User],
+      }),
+      inject: [ConfigService],
     })
   ]
 })
