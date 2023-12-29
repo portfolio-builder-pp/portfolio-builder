9f22dd2e757c41bdbc4a0bb6c0becc2c9846e39b
Jan Sobanski <sobanski.jan2@gmail.com>
2023-12-29 23:45:26 +0100

    Add page errors

--- libs/shared/types/src/errors.ts
+++ libs/shared/types/src/errors.ts
@@ -1,4 +1,8 @@
 export enum AuthErrors {
   InvalidEmailOrPassword = 'invalid-email-or-password',
-  EmailAlreadyInUse = 'email-alread-in-use',
+  EmailAlreadyInUse = 'email-already-in-use',
+}
+
+export enum PageErrors {
+  PageDoesNotExist = 'page-does-not-exist',
 }
