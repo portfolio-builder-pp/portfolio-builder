3dd8eaa64b6c319e13d28bf0eb1c30119ef61270
Jan Sobanski <sobanski.jan2@gmail.com>
2024-01-06 15:59:10 +0100

    Add blog post dtos

---
+++ libs/shared/types/src/blog-post.ts
@@ -0,0 +1,36 @@
+import { WithDefaultValues } from './helpers';
+import { InternalUserDto, UserDto } from './user';
+
+export enum BlogPostStatus {
+  Draft = 'draft',
+  Published = 'published',
+  Archived = 'archived',
+}
+
+export interface BlogPostDto {
+  id: string;
+  title: string;
+  content: string;
+  order: number;
+  status: BlogPostStatus;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  author: UserDto;
+}
+
+export interface InternalBlogPostDto extends BlogPostDto {
+  author: InternalUserDto;
+}
+
+export type CreateBlogPostDto = WithDefaultValues<
+  Omit<
+    BlogPostDto,
+    'id' | 'publishedAt' | 'createdAt' | 'updatedAt' | 'author'
+  >,
+  'status'
+>;
+export type UpdateBlogPostDto = {
+  id: string;
+  details: Partial<CreateBlogPostDto>;
+};
