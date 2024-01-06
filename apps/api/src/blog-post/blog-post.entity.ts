fffcf027317447e0af18a51dedfcd31418362d4e
Jan Sobanski <sobanski.jan2@gmail.com>
2024-01-06 16:02:07 +0100

    Define blog post module & logic

---
+++ apps/api/src/blog-post/blog-post.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { BlogPostDto, BlogPostStatus } from '@portfolio-builder/shared-types';
+import { User } from '../user';
+
+@Entity()
+export class BlogPost implements BlogPostDto {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  order: number;
+
+  @Column({
+    type: 'enum',
+    enum: BlogPostStatus,
+    default: BlogPostStatus.Draft,
+  })
+  status: BlogPostStatus;
+
+  @Column({ nullable: true })
+  publishedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.blogPosts, { onDelete: 'SET NULL' })
+  author: User;
+}
