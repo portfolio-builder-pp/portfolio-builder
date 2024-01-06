603b1bc99af91b18d8bb1b01cb31b51880020816
Jan Sobanski <sobanski.jan2@gmail.com>
2024-01-06 15:57:20 +0100

    Add reusable date schema

--- libs/shared/validation/src/utils.ts
+++ libs/shared/validation/src/utils.ts
@@ -3,3 +3,10 @@ import { z } from 'zod';
 export const idSchema = z
   .object({ id: z.string().uuid() })
   .strip();
+
+export const dateSchema = z
+  .object({
+    createdAt: z.date(),
+    updatedAt: z.date(),
+  })
+  .strip();
