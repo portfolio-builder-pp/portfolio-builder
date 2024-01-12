fb01e13ee8a546d3b55b47384183c311e66e3f96
Ryszard Poznański <ryszard.poznanski0@gmail.com>
2024-01-12 14:55:46 +0100

    feat: blog post update page

---
+++ apps/dashboard/src/modules/blog/pages/Update.page.tsx
@@ -0,0 +1,47 @@
+import { BlogPostForm, FieldValues } from '../forms/BlogPost.form';
+import { trpc } from '../../../shared/trpc-query';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UpdateBlogPostDto } from '@portfolio-builder/shared-types';
+
+export function BlogUpdatePage() {
+  const { id } = useParams<'id'>();
+  const navigate = useNavigate();
+  if (!id) return null;
+
+  const { data, status } = trpc.blogPost.findById.useQuery(
+    { id: id },
+    { enabled: !!id }
+  );
+
+  const { mutateAsync } = trpc.blogPost.update.useMutation({
+    onSuccess() {
+      navigate('/dashboard/blog');
+    },
+  });
+
+  if (status === 'loading') return null;
+  if (status === 'error' || !data) return <div>błąd pobierania</div>;
+
+  const defaultValues: FieldValues = {
+    content: data.content,
+    description: data.description,
+    order: data.order,
+    status: data.status,
+    title: data.title,
+  };
+
+  return (
+    <BlogPostForm
+      defaultValues={defaultValues}
+      onSubmit={async (values) => {
+        const payload: UpdateBlogPostDto = {
+          id,
+          details: {
+            ...values,
+          },
+        };
+        await mutateAsync(payload);
+      }}
+    />
+  );
+}
