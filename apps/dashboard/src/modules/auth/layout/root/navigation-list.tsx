7b4b99da310dc513a863e1b56d817ddf8833e417
Jan Sobanski <sobanski.jan2@gmail.com>
2024-01-13 00:56:26 +0100

    Add empty portfolio and properties pages

--- apps/dashboard/src/modules/auth/layout/root/navigation-list.tsx
+++ apps/dashboard/src/modules/auth/layout/root/navigation-list.tsx
@@ -2,7 +2,13 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { AccountBox, ContactMail, Article } from '@mui/icons-material';
+import {
+  AccountBox,
+  ContactMail,
+  Article,
+  HistoryEdu,
+  AddBox,
+} from '@mui/icons-material';
 import { To, useNavigate } from 'react-router-dom';
 import { Fragment, ReactNode } from 'react';
 import { UserDto, UserRole } from '@portfolio-builder/shared-types';
@@ -51,6 +57,18 @@ const navigationListItems: NavigationListItem[] = [
     icon: <ContactMail />,
     title: 'Contact Details',
     to: '/dashboard/contact',
+    roles: [UserRole.Admin, UserRole.Moderator],
+  },
+  {
+    icon: <HistoryEdu />,
+    title: 'Portfolio',
+    to: '/dashboard/portfolio',
+    roles: [UserRole.Admin, UserRole.Moderator],
+  },
+  {
+    icon: <AddBox />,
+    title: 'Properties',
+    to: '/dashboard/property',
     roles: [UserRole.Admin, UserRole.Moderator],
   },
   {
