b2e34280a63c5abacf0ac326ad305de983262589
Jan Sobanski <sobanski.jan2@gmail.com>
2024-01-28 20:20:20 +0100

    Add test delete modal to the contact table

--- apps/dashboard/src/modules/contact/tables/Contact.table.tsx
+++ apps/dashboard/src/modules/contact/tables/Contact.table.tsx
@@ -1,27 +1,82 @@
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridColDef, GridDeleteIcon } from '@mui/x-data-grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
 import { ContactDetailsDto } from '@portfolio-builder/shared-types';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 import { trpc } from '../../../shared/trpc-query';
+import { useMemo, useState } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import { Button } from '@mui/material';
 
 export const ContactsTable = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalId, setModalId] = useState<string>('');
+
+  const { mutateAsync } = trpc.contactDetails.remove.useMutation();
+
+  const handleOpen = (id: string) => {
+    setModalId(id);
+    setModalOpen(true);
+  }
+
+  const handleClose = (accept: boolean) => {
+    if (accept && modalId) {
+      mutateAsync({
+        id: modalId,
+      });
+    }
+
+    setModalOpen(false);
+    setModalId('');
+  }
+
+  const navigate = useNavigate();
   const contactDetails = trpc.contactDetails.findAll.useQuery();
+  const columns = useMemo(() => getColumns({ navigate, handleOpen }), [navigate]);
+
   return (
-    <DataGrid
-      loading={contactDetails.isLoading}
-      rows={contactDetails.data || []}
-      columns={columns}
-      initialState={{
-        pagination: {
-          paginationModel: {
-            pageSize: 10,
+    <>
+      <DataGrid
+        loading={contactDetails.isLoading}
+        rows={contactDetails.data || []}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
           },
-        },
-      }}
-      pageSizeOptions={[10, 20, 50, 100]}
-    />
+        }}
+        pageSizeOptions={[10, 20, 50, 100]}
+      />
+      <Dialog
+        open={modalOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Are you sure that you want to delete this item?
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={() => handleClose(false)}>Disagree</Button>
+          <Button onClick={() => handleClose(true)} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
-const columns: GridColDef<ContactDetailsDto>[] = [
+const getColumns = ({
+  navigate,
+  handleOpen,
+}: {
+  navigate: NavigateFunction;
+  handleOpen: (id: string) => void;
+}): GridColDef<ContactDetailsDto>[] => [
   {
     field: 'title',
     headerName: 'Title',
@@ -51,5 +106,25 @@ const columns: GridColDef<ContactDetailsDto>[] = [
     type: 'boolean',
     valueGetter: ({ row: { socialMediaLinks } }) =>
       socialMediaLinks && Object.values(socialMediaLinks).length > 0,
+  },
+  {
+    field: 'actions',
+    type: 'actions',
+    getActions(params) {
+      return [
+        <GridActionsCellItem
+          label="Edit"
+          icon={<EditIcon />}
+          showInMenu
+          onClick={() => navigate(`${params.row.id}/update`)}
+        />,
+        <GridActionsCellItem
+          label="Edit"
+          icon={<GridDeleteIcon />}
+          showInMenu
+          onClick={() => handleOpen(params.row.id)}
+        />,
+      ];
+    },
   },
 ];
